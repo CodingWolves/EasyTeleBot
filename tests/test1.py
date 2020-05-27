7108d1b87992f1e622d3aca9d720ecd9247b18d3
idozahavy <idozahavy@users.noreply.github.com>
2020-05-27 10:58:58 +0300

    fix

--- tests/test1.py
+++ tests/test1.py
@@ -2,4 +2,7 @@ from EasyBot.EasyRun import EasyBot
 
 file = open('json_file.json')
 bot = EasyBot(file)
-bot.app.run()
+
+if __name__ == '__main__':
+    print('main function started')
+    bot.app.run(threaded=True)
