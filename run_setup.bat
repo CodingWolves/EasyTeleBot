60247a6787ce69cbaf552f4e41d076b49fd83b5f
idozahavy <idozahavy@users.noreply.github.com>
2020-05-27 19:19:13 +0300

    batch build

--- run_setup.bat
+++ run_setup.bat
@@ -1,1 +1,11 @@
-python setup.py sdist bdist_wheel
+python -m pip install --user --upgrade setuptools wheel
+
+@echo off
+echo.
+echo.
+echo.
+echo are you sure you want to create a new build? y-yes , otherwise no
+echo.
+set /p "answer=____"
+if "%answer%"=="Y" python setup.py sdist bdist_wheel
+pause
