46752c5002b63855c4df53f7c4b322e70ea17391
idozahavy <idozahavy@users.noreply.github.com>
2020-05-27 19:18:38 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="IdoZahavy", # Replace with your own username
+    name="EasyBot",  # Replace with your own username
     version="0.0.1",
     author="Ido Zahavy",
     author_email="idozahavy@gmail.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/idozahavy/EasyBot",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
