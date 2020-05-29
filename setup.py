fa48e892974e11c010cc1a601c6b9c04790795e7
idozahavy <idozahavy@users.noreply.github.com>
2020-05-29 19:24:36 +0300

    install_requires add

--- setup.py
+++ setup.py
@@ -4,20 +4,22 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="EasyTeleBot",  # Replace with your own username
+    name="EasyTeleBot",
     version="0.0.1",
     author="Ido Zahavy",
     author_email="idozahavy@gmail.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/idozahavy/EasyBot",
+    url="https://github.com/idozahavy/EasyTeleBot",
     packages=setuptools.find_packages(),
     classifiers=[
+        "Development Status :: 3 - Alpha",  # 3 - Alpha , 4 - Beta , 5 - Production/Stable
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=["python-telegram-bot", "flask"]
 )
 # https://packaging.python.org/tutorials/packaging-projects/
