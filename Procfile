55bbae61d02f6d78993277cd3a64a6812976a2e2
idozahavy <idozahavy@users.noreply.github.com>
2020-06-28 15:38:32 +0300

    second gunicorn test

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn --chdir tests test1:app --preload
+web: gunicorn --chdir tests print_sec:aang --preload
---
+++ tests/print_sec.py
@@ -0,0 +1,10 @@
+from time import sleep
+
+
+class momo:
+    def run(self):
+        sleep(1)
+        print("1 second passed")
+
+
+aang = momo()
