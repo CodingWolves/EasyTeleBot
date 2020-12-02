aaa5644c2a3019baa55aa5141a146b0401e06950
idozahavy@gmail.com <idozahavy@gmail.com>
2020-12-02 23:10:45 +0200

    Update server_db.py

--- tests/server_db.py
+++ tests/server_db.py
@@ -9,20 +9,21 @@ except:
     print("Server Already Started")
     exit()
 
-chats = {}
-messages = {}
+if __name__ == "__main__":
+    chats = {}
+    messages = {}
 
-sock.listen(4)
-while True:
-    connection, client_address = sock.accept()
-    print(f"Connection to {client_address}")
-    messages[client_address] = ""
+    sock.listen(4)
     while True:
-        portion = connection.recv(8)
-        print(portion)
-        if portion:
-            decoded_portion = portion.decode('utf-8')
-            messages[client_address] += decoded_portion
-        else:
-            print("Got '"+messages[client_address]+"'")
-            break
+        connection, client_address = sock.accept()
+        print(f"Connection to {client_address}")
+        messages[client_address] = ""
+        while True:
+            portion = connection.recv(8)
+            print(portion)
+            if portion:
+                decoded_portion = portion.decode('utf-8')
+                messages[client_address] += decoded_portion
+            else:
+                print("Got '"+messages[client_address]+"'")
+                break
